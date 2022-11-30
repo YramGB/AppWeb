d986fdec7585e4c4246d52e298cabd0a95c184e3
YramGB <angelyramgb@gmail.com>
2022-11-29 20:50:09 -0600

    first commit

---
+++ json/question.js
@@ -0,0 +1,102 @@
+data = [
+    {
+        question:"¿Qué piloto tiene mayor número de mundiales ganados?",
+        answers:{
+            a:"Schumacher",
+            b:"Alonso",
+            c:"Vettel",
+            d:"Hamilton"
+        },
+        correctAnswer:"a"
+    },
+    {
+        question: "¿Cual es el único piloto español con mundiales ganados?",
+        answers:{
+            a:"Sainz",
+            b:"Pedro de la Rosa",
+            c:"Alonso",
+            d:"Picket"
+        },
+        correctAnswer:"c"
+    },
+    {
+        question: "De los siguientes pilotos, ¿quién que no pilota o no ha pilotado en la escuderia Red Bull?",
+        answers:{
+            a:"Mark Webber",
+            b:"Charles Leclerc",
+            c:"Sebastian Vettel",
+            d:"Alexander Albon"
+        },
+        correctAnswer:"b"
+    },
+    {
+        question: "¿Quién ha sido el piloto más joven en su debut?",
+        answers:{
+            a:"Ricciardo",
+            b:"Hamilton",
+            c:"Alonso",
+            d:"Senna"
+        },
+        correctAnswer:"c"
+    },
+    {
+        question: "¿Qué escuderia no se encuentra actualmente en la f1?",
+        answers:{
+            a:"Audi",
+            b:"Toro Rosso",
+            c:"McLaren",
+            d:"Williams"
+        },
+        correctAnswer:"a"
+    },
+    {
+        question: "¿A escudería pertenece el piloto Lando Norris?",
+        answers:{
+            a:"Ferrari",
+            b:"McLaren",
+            c:"Alfa romeo",
+            d:"Red bull"
+        },
+        correctAnswer:"b"
+    },
+    {
+        question: "¿Qué piloto es de nacionalidad Frances?",
+        answers:{
+            a:"Pierre Gasly",
+            b:"Carlos Sainz",
+            c:"Lance Stroll",
+            d:"Kimi Raikkonen"
+        },
+        correctAnswer:"a"
+    },
+    {
+        question: "¿Qué piloto fué nombrado 'el hombre en llamas'?",
+        answers:{
+            a:"Romain Grosjean",
+            b:"Nicolas Latifi",
+            c:"Max Verstapen",
+            d:"Lewis Hamilton"
+        },
+        correctAnswer:"a"
+    },
+    {
+        question: "¿Qué piloto ganó más campeonatos en la parrilla de 2019?",
+        answers:{
+            a:"Vettel",
+            b:"Hamilton",
+            c:"Senna",
+            d:"Perez"
+        },
+        correctAnswer:"b"
+    },
+    {
+        question: "¿Qué piloto es mexicano?",
+        answers:{
+            a:"Sergio Perez",
+            b:"Robert Kubica",
+            c:"Pierre Gasly",
+            d:"Charles Leclerc"
+        },
+        correctAnswer:"a"
+    }
+];
